1eb097e7dc67fdc9f8940d8cb374eacbb3f95bdf
andres <lp.carlosorjuela@gmail.com>
2025-07-15 22:20:45 -0400

    init

---
+++ back/src/main/java/com/futbolapp/back/models/Prediction.java
@@ -0,0 +1,10 @@
+package com.futbolapp.back.models;
+
+public class Prediction {
+    private String id;
+    private String matchId;
+    private String userId;
+    private int homeGoals;
+    private int awayGoals;
+    //private MatchResult result; // ENUM: WIN, LOSE, DRAW
+}
